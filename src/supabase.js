52dade030ce83b73b8bcd6bbc906ce86271038f2
rpulicen <rod.puliceno@gmail.com>
2026-03-01 23:12:40 -0800

    Updated supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = 'https://esdxzoyeuoarrxuxjxnq.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzZHh6b3lldW9hcnJ4dXhqeG5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5MDIyMzAsImV4cCI6MjA1NjQ3ODIzMH0.m5lO0W0KscW7U999y7fO3V8P1uLpLqG9f_Xn-yG5-g8';
+
+console.log('Using Project Ref: esdxzoyeuoarrxuxjxnq');
+
 export const supabase = createClient(
-  'https://esdxzoyeuoarrxuxjxnq.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzZHh6b3lldW9hcnJ4dXhqeG5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5MDIyMzAsImV4cCI6MjA1NjQ3ODIzMH0.m5lO0W0KscW7U999y7fO3V8P1uLpLqG9f_Xn-yG5-g8',
+  supabaseUrl,
+  supabaseAnonKey,
   {
     auth: {
       persistSession: true,
